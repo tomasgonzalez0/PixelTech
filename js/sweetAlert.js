b76f129b84ea4c7fde3b0d1986d40426d1392fae
ThomasGz06 <116316891+ThomasGz06@users.noreply.github.com>
2022-10-20 17:28:54 -0500

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,10 @@
+function proceso() {
+    Swal.fire({
+    title: "Curso en proceso",
+    text: "Este curso aun esta en proceso sin embargo puedes ir viendo otros que tengamos disponbles",
+    icon: 'info',
+    footer: 'Cursos disponibles: <a href="../pages/electronica.html">Electronica</a> & <a href="../pages/binarios.html">Binarios</a>',
+    stopKeydownPropagation: false,
+    confirmButtonColo: '#fc00ff',
+});
+}
